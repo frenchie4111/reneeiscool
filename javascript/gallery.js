12e9ff40b811ad5370bd7d4a7a560f4d8cfaa999
Michael Lyons <mdl0394@gmail.com>
2015-03-18 00:19:02 -0700

    Moved gallery into it's own file, and refactored it so you could have more than one on a page

---
+++ javascript/gallery.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright of Mark One Lifestyle Inc.
+ *
+ * Authors:
+ *     - Mike Lyons (m@mkone.co)
+ */
+
+var Gallery;
+
+(function() {
+    'use strict';
+
+    Gallery = function( image_array, image_selector, left_selector, right_selector, dots_container_selector ) {
+        this.image_array = image_array;
+        this.image = $( image_selector );
+        this.left = $( left_selector );
+        this.right = $( right_selector );
+        this.dots_container = $( dots_container_selector );
+
+        if( !this.image || !this.left || !this.right || !this.dots_container ) throw Error( 'Missing' );
+
+        this.position = 0;
+
+        var _this = this;
+
+        this.left.click( function() {
+            _this.handleClick( -1 );
+        } );
+
+        this.right.click( function() {
+            _this.handleClick( 1 );
+        } );
+
+        this.updateDots();
+    };
+
+    Gallery.prototype.updateDots = function() {
+        var dots = this.dots_container.children( '.project_stills_circle' );
+        var count = this.image_array.length;
+
+        if( dots.length < count ) {
+            for( var i = dots.length; i < count; i++ )
+            {
+                var new_dot = $( '<div></div>' ).addClass( 'project_stills_circle' );
+                this.dots_container.append( new_dot );
+            }
+            dots = this.dots_container.children( '.project_stills_circle' )
+        }
+
+        dots.removeClass( 'project_stills_circle_filled' );
+        $( dots[ this.position ] ).addClass( 'project_stills_circle_filled' );
+    };
+
+    Gallery.prototype.performTransition = function( next ) {
+        var _this = this;
+
+        this.position = next;
+
+        $( _this.image ).fadeTo( 'fast', 0, function() {
+            _this.image.attr( 'src', gallery_images[ next ] );
+            $( _this.image ).fadeTo( 'fast', 1 );
+        } );
+
+        this.updateDots();
+    };
+
+    /**
+     * Handles an arrow click, pass in direction
+     * @param direction the direction of click. 1 = right, -1 = left
+     */
+    Gallery.prototype.handleClick = function( direction ) {
+        var next = this.position + direction;
+        if( next >= 0 && next < this.image_array.length ) {
+            this.performTransition( next );
+        }
+    };
+
+})();
--- javascript/main.js
+++ javascript/main.js
@@ -26,64 +26,6 @@ function OpenResume() {
         }
     };
 
-    var _updateDots = function( val, count ) {
-        var dot_container = $( '.project_stills_circles_container' );
-        if( !dot_container ) return;
-
-        var dots = dot_container.children( '.project_stills_circle' );
-
-        if( dots.length < count ) {
-            for( var i = dots.length; i < count; i++ )
-            {
-                var new_dot = $( '<div></div>' ).addClass( 'project_stills_circle' );
-                dot_container.append( new_dot );
-            }
-            dots = dot_container.children( '.project_stills_circle' )
-        }
-
-        dots.removeClass( 'project_stills_circle_filled' );
-        $( dots[ val ] ).addClass( 'project_stills_circle_filled' );
-    };
-
-    var _galleryTransition = function( img, next ) {
-        $( img ).fadeTo( 'fast', 0, function() {
-            img.attr( 'src', gallery_images[ next ] );
-            $( img ).fadeTo( 'fast', 1 );
-        } );
-
-        _updateDots( next );
-    };
-
-    var _gallery = function() {
-        var gallery_box = $( '.project_stills_still_box' );
-        if( !gallery_box ) return;
-
-        var gallery_image = gallery_box.children( 'img' );
-        var left = $( '#left_arrow' );
-        var right = $( '#right_arrow' );
-        if( !gallery_image || !left || !right ) return;
-
-        left.click( function() {
-            console.log( gallery_position );
-            if( gallery_position <= 0 ) return;
-
-            var last = gallery_position;
-            gallery_position --;
-
-            _galleryTransition( gallery_image, gallery_position );
-        } );
-
-        right.click( function() {
-            console.log( gallery_position );
-            if( gallery_position >= gallery_images.length - 1 ) return;
-
-            var last = gallery_position;
-            gallery_position ++;
-
-            _galleryTransition( gallery_image, gallery_position );
-        } );
-    };
-
     $( document ).ready( function() {
         $( document ).scroll( _toggleTop );
 
@@ -100,9 +42,6 @@ function OpenResume() {
         }, function() {
             $( this ).children( '.work_item_hover' ).css( 'visibility', 'hidden' );
         } );
-
-        _gallery();
-        _updateDots( gallery_position, gallery_images.length );
     } );
 
 })();
